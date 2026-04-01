bd1dbedc8dfbd8419148b45c660e9c3193a1b733
Sergey <sedorofeevd@ozon.ru>
2026-04-02 02:09:47 +0300

    [SEARCHADS-20191] fix Заказано на сумму сортировка

--- apps/api/scripts/migrations/00016_trim_arena_storage.sql
+++ apps/api/scripts/migrations/00016_trim_arena_storage.sql
@@ -1,2 +1,7 @@
+-- +goose Up
 ALTER TABLE arena_submissions
-    DROP COLUMN IF EXISTS code;
+  DROP COLUMN IF EXISTS code;
+
+-- +goose Down
+ALTER TABLE arena_submissions
+  ADD COLUMN IF NOT EXISTS code TEXT NOT NULL DEFAULT '';
